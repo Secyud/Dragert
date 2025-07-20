bd6e9604693facc54282a72ca1d68158106654c1
laotu <2427796523@qq.com>
2025-07-20 13:50:05 +0800

    recode

---
+++ fonts/src/font-builder.ts
@@ -0,0 +1,65 @@
+﻿import {SVGIcons2SVGFontStream} from "svgicons2svgfont";
+import * as svg2ttf from "svg2ttf";
+import ttf2woff2 from "ttf2woff2";
+import {join} from "node:path";
+import {fs} from "../../utils/src/file-stream";
+
+interface ISvgIconData {
+    name: string;
+    unicode: string;
+    path: string;
+}
+
+
+export class FontBuilder {
+    data: ISvgIconData[];
+
+    addSvg(svgData: ISvgIconData) {
+        this.data.push(svgData);
+    }
+
+
+    async buildWoff2Font(inputTtfPath: string, outputWoff2Path: string) {
+        const buffer = await fs.readFile(inputTtfPath, {encoding: "utf-8"});
+        const woff2 = ttf2woff2(buffer);
+        await fs.writeFile(outputWoff2Path, woff2, {encoding: "utf-8"});
+        console.log(`Woff2 icon successfully created!(${outputWoff2Path})`);
+    }
+
+    async buildTtfFont(inputSvgPath: string, outputTtfPath: string) {
+        const buffer = await fs.readFile(inputSvgPath, {encoding: 'utf-8'});
+        const ttf = svg2ttf(typeof buffer === "string" ? buffer : buffer.toString());
+        await fs.writeFile(outputTtfPath, Buffer.from(ttf.buffer), {encoding: "utf-8"});
+    }
+
+    async buildSvgFont(outputPath: string, fontName: string) {
+        const fontStream = new SVGIcons2SVGFontStream({fontName: fontName});
+        const outputSvgFontPath = join(outputPath, fontName + ".svg");
+        const ws = fs.createWriteStream(outputSvgFontPath);
+
+        fontStream.pipe(ws);
+
+        let css = ".si{";
+
+        for (const icon of this.data) {
+            let glyph = fs.createReadStream(icon.path);
+            glyph["metadata"] = {
+                unicode: [icon.unicode],
+                name: icon.name,
+            }
+            css += `&.b.${icon}::before {content: '${icon.unicode}';}`;
+            css += `&.a.${icon}::after {content: '${icon.unicode}';}`;
+            fontStream.write(glyph);
+        }
+
+        fontStream.end();
+
+        css += '}';
+
+        const cssFileOutputPath = join(outputPath, fontName + ".css");
+
+        await fs.writeFile(cssFileOutputPath, css, {encoding: "utf-8"});
+
+        console.log(`Svg Font css create successfully!(${cssFileOutputPath})`);
+    }
+}
---
+++ utils/src/file-stream.ts
@@ -0,0 +1,25 @@
+﻿import {Abortable} from "events";
+import * as fileStream from "node:fs"
+
+export const fs = {
+        readFile: (
+            path: fileStream.PathOrFileDescriptor,
+            options?: (fileStream.ObjectEncodingOptions & Abortable) |
+                undefined | null) => new Promise((resolve: (data: string | NonSharedBuffer) => void, reject) => {
+            fileStream.readFile(path, options, (err, data) => {
+                if (err) reject(err); else resolve(data);
+            });
+        }),
+        writeFile: (path: fileStream.PathOrFileDescriptor,
+                    data: string | NodeJS.ArrayBufferView,
+                    options?: fileStream.WriteFileOptions) => new Promise((resolve, reject) => {
+            fileStream.writeFile(path, data, options, (err) => {
+                if (err) reject(err); else resolve(undefined);
+            });
+        }),
+        createWriteStream: (path: fileStream.PathLike, options?: BufferEncoding) =>
+            fileStream.createWriteStream(path, options),
+        createReadStream: (path: fileStream.PathLike, options?: BufferEncoding) =>
+            fileStream.createReadStream(path, options),
+    }
+;
