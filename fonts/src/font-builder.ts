fd566d9497100347bd7da860c43051bf703919a4
laotu <2427796523@qq.com>
2026-04-01 15:32:49 +0800

    update code

--- fonts/src/font-builder.ts
+++ fonts/src/font-builder.ts
@@ -3,8 +3,7 @@ import {SVGIcons2SVGFontStream} from "svgicons2svgfont";
 import svg2ttf from "svg2ttf";
 import ttf2woff2 from "ttf2woff2";
 import {join} from "node:path";
-import {fs} from "../../utils/src/file-stream";
-import {readFile, writeFile} from 'node:fs/promises';
+import {FileHelper} from "../../utils/src/file-helper";
 
 interface ISvgIconData {
     name: string;
@@ -20,23 +19,23 @@ export class FontBuilder {
     }
 
     async buildWoff2Font(inputTtfPath: string, outputWoff2Path: string) {
-        const buffer = await fs.readFile(inputTtfPath);
+        const buffer = await FileHelper.readFile(inputTtfPath);
         const woff2 = ttf2woff2(buffer as NonSharedBuffer);
-        await fs.writeFile(outputWoff2Path, woff2, {encoding: "utf-8"});
+        await FileHelper.writeFile(outputWoff2Path, woff2, {encoding: "utf-8"});
         console.log(`Woff2 icon successfully created!(${outputWoff2Path})`);
     }
 
     async buildTtfFont(inputSvgPath: string, outputTtfPath: string) {
-        const buffer = await fs.readFile(inputSvgPath, {encoding: 'utf-8'});
+        const buffer = await FileHelper.readFile(inputSvgPath, {encoding: 'utf-8'});
         const ttf = svg2ttf(typeof buffer === "string" ? buffer : buffer.toString());
-        await fs.writeFile(outputTtfPath, Buffer.from(ttf.buffer), {encoding: "utf-8"});
+        await FileHelper.writeFile(outputTtfPath, Buffer.from(ttf.buffer), {encoding: "utf-8"});
     }
 
     buildSvg(outputPath: string, fontName: string) {
         return new Promise<void>((resolve, reject) => {
 
             const fontStream = new SVGIcons2SVGFontStream({fontName: fontName});
-            const ws = fs.createWriteStream(outputPath);
+            const ws = FileHelper.createWriteStream(outputPath);
 
             fontStream.pipe(ws)
                 .on('finish', function () {
@@ -49,7 +48,7 @@ export class FontBuilder {
                 });
 
             for (const icon of this.data) {
-                let glyph = fs.createReadStream(icon.path);
+                let glyph = FileHelper.createReadStream(icon.path);
                 glyph["metadata"] = {
                     unicode: [icon.unicode],
                     name: icon.name,
@@ -63,7 +62,7 @@ export class FontBuilder {
 
     async buildSvgFontCss(outputPath: string, fontClass: string) {
 
-        let css = `.${fontClass}{`;
+        let css = `${fontClass}{`;
 
         for (const icon of this.data) {
             css += `&.${icon.name}::after {content: '${icon.unicode}';}`;
@@ -73,7 +72,7 @@ export class FontBuilder {
 
         const cssFileOutputPath = join(outputPath);
 
-        await fs.writeFile(cssFileOutputPath, css, {encoding: "utf-8"});
+        await FileHelper.writeFile(cssFileOutputPath, css, {encoding: "utf-8"});
 
         console.log(`Svg Font css create successfully!(${cssFileOutputPath})`);
     }
---
+++ utils/src/file-helper.ts
@@ -0,0 +1,34 @@
+﻿import {Abortable} from "events";
+import * as fs from "node:fs"
+
+export class FileHelper {
+    static readFile(
+        path: fs.PathOrFileDescriptor,
+        options?: (fs.ObjectEncodingOptions & Abortable) |
+            undefined | null) {
+        return new Promise((resolve: (data: string | NonSharedBuffer) => void, reject) => {
+            fs.readFile(path, options, (err, data) => {
+                if (err) reject(err); else resolve(data);
+            })
+        })
+
+    }
+
+    static writeFile(path: fs.PathOrFileDescriptor,
+                     data: string | NodeJS.ArrayBufferView,
+                     options?: fs.WriteFileOptions) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path, data, options, (err) => {
+                if (err) reject(err); else resolve(undefined);
+            });
+        })
+    }
+
+    static createWriteStream(path: fs.PathLike, options?: BufferEncoding) {
+        return fs.createWriteStream(path, options);
+    }
+
+    static createReadStream(path: fs.PathLike, options?: BufferEncoding) {
+        return fs.createReadStream(path, options);
+    }
+}
